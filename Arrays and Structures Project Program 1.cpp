c4c67e4f31f2bca2f0395004764aa81212c2912c
Roberto Fitzhugh <rgf274@email.vccs.edu>
2024-02-26 15:17:37 -0500

    In Progress

--- Arrays and Structures Project Program 1.cpp
+++ Arrays and Structures Project Program 1.cpp
@@ -7,6 +7,8 @@ using namespace std;
 int main()
 {
 	string RunnerNames[5] = {"Jason", "Samantha", "Ravi", "Sheila", "Ankit"};
+	string RunnerDays[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+	double RunnerDistance[7];
 
 	cout << RunnerNames[0] << endl;
 
