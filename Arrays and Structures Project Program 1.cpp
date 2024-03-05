e413ab703d26d3b8684dc98e83f74a7548f135d3
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-03-05 15:15:41 -0500

    In Progress

--- Arrays and Structures Project Program 1.cpp
+++ Arrays and Structures Project Program 1.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <fstream>
 
 using namespace std;
+
+// Reads the names of runners from the file.
+// Pre-Condition: Finds existing file and reads and stores names of runners.
+// Post-Condition: Holds the names for the next function to use.
+void RunnerNames(int RunnerData[][7], int RunnerNames);
 
 int main()
 {
@@ -12,4 +18,17 @@ int main()
 	
 
 	return 0;
+}
+
+void RunnerNames(int RunnerData[][7], int RunnerNames)
+{
+	ifstream myiFile;
+
+	myiFile.open("Runner Data.txt");
+
+	for (i = 0; i < 5; i++)
+
+
+
+		
 }
