996ff2b0b9cdca86e98ec372bbb8dd487135570e
Roberto Fitzhugh <thelonesaiyan.xboxlive@yahoo.com>
2024-03-05 14:58:11 -0500

    In Progress

--- Arrays and Structures Project Program 1.cpp
+++ Arrays and Structures Project Program 1.cpp
@@ -6,11 +6,10 @@ using namespace std;
 
 int main()
 {
-	string RunnerNames[5] = {"Jason", "Samantha", "Ravi", "Sheila", "Ankit"};
-	string RunnerDays[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-	double RunnerDistance[7];
+	string RunnerNames[5];
+	string RunnerData[5][7];
 
-	cout << RunnerNames[0] << endl;
+	
 
 	return 0;
 }
